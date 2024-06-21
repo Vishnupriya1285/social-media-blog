d097aad9cece0f9579844fa265e024e784723fdd
MeVishnupriya <vishnupriya.singamaneni@gmail.com>
2024-06-21 14:21:08 -0400

    Initial commit for Social Media Blog

---
+++ src/main/java/com/learning/springboot/social_media_blog/SocialMediaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.social_media_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBlogApplication.class, args);
+	}
+
+}
