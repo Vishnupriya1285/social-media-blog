c0b248cba01940f44f9f8e01dd0ac66e568a0733
MeVishnupriya <vishnupriya.singamaneni@gmail.com>
2024-06-21 14:44:19 -0400

    Initial project setup files of Social Media Blog App for main branch

---
+++ src/main/java/com/learning/springboot/socialmedia_blog_app/SocialmediaBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.socialmedia_blog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springboot/socialmedia_blog_app/SocialmediaBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.socialmedia_blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
